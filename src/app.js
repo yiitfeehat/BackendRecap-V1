987f24d20955f789e73a83be7bd6dd12c285f3a4
yiitfeehat <yiitferhat@gmail.com>
2026-01-12 18:32:11 +0300

    added -helmet -express-rate-limit

--- src/app.js
+++ src/app.js
@@ -4,12 +4,24 @@ const authRouter = require('./routes/auth.routes');
 const productRouter = require('./routes/product.routes');
 const errorHandler = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+
+
 const app = express();
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 dakika
+    max: 100, // Limit
+    message: "Çok fazla istek attınız, lütfen biraz bekleyin."
+});
+
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(helmet());
+app.use(limiter);
 
 
 /**---------- ROUTES ---------*/
